f5e7df50409bd634047d8cd90e7353e4529c3348
Abusolo94 <rickysolo44@gmail.com>
2026-05-25 20:37:23 +0300

    Check Groq env

--- src/config/openai.js
+++ src/config/openai.js
@@ -14,7 +14,7 @@ import OpenAI from "openai";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log(process.env.GROQ_API_KEY ? "KEY FOUND" : "NO KEY");
 export const openai = new OpenAI({
   apiKey: process.env.GROQ_API_KEY,
   baseURL: "https://api.groq.com/openai/v1",
